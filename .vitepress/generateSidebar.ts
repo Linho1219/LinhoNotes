6ab4be971a448b643fcf175a70c73b12605ca594
Linho <lhp@linho.cc>
2024-10-27 02:32:29 +0800

    优化侧边栏生成逻辑

--- .vitepress/generateSidebar.ts
+++ .vitepress/generateSidebar.ts
@@ -1,97 +1,65 @@
 import fs from "fs";
 import path from "path";
-import yaml from "js-yaml";
 import { DefaultTheme } from "vitepress";
 
-type IndexStructure = {
-  layout: string;
-  hero: {
-    name: string;
-    text?: string;
-    tagline?: string;
-    actions?: {
-      theme: string;
-      text: string;
-      link: string;
-    }[];
-  };
-  features: {
-    title: string;
-    detail: string;
-    link: string;
-  }[];
-};
+const ignProjects = [".vitepress", ".github", ".git", "node_modules", "public"];
+const ignDirs = ["images"];
+const ignFiles = ["index.md"];
 
-export default function sidebar() {
-  const sidebarObj = {};
-  const indexMD = <IndexStructure>yaml.load(
-    fs
-      .readFileSync("index.md", "utf-8")
-      .trim()
-      .replace(/\n---$/, "")
-  );
-  indexMD.features.forEach(({ link }) => {
-    sidebarObj[link] = generateSidebar(link);
-  });
-  return sidebarObj;
+function compareFileName(a: string, b: string) {
+  const matchA = a.match(/^((\d+)(\.(\d+))?)\s/),
+    matchB = b.match(/^((\d+)(\.(\d+))?)\s/);
+  if (matchA === null || matchB === null) return a.localeCompare(b);
+  const NumberWithoutNaN = (str: string) =>
+    isNaN(Number(str)) ? 0 : Number(str);
+  const indexA = Number(matchA[2]) * 1000 + NumberWithoutNaN(matchA[4]),
+    indexB = Number(matchB[2]) * 1000 + NumberWithoutNaN(matchB[4]);
+  return indexA - indexB;
 }
 
-function getMarkdownTitle(filePath: string): string {
-  // const content = fs.readFileSync(filePath, "utf-8");
-  // const match = content.match(/title:\s*(.*)/);
-  // return match ? match[1] : path.basename(filePath, ".md");
-  return path.basename(filePath, ".md");
-}
+const pangu = (str: string) =>
+  str
+    .replace(/([0-9A-Za-z&+\-])([\u4e00-\u9fff])/g, "$1 $2")
+    .replace(/([\u4e00-\u9fff])([0-9A-Za-z&+\-])/g, "$1 $2");
 
 function generateSidebar(dir: string): DefaultTheme.SidebarItem[] {
-  const sidebar: DefaultTheme.SidebarItem[] = [];
-  function compareFileName(a: string, b: string) {
-    const matchA = a.match(/^((\d+)(\.(\d+))?)\s/),
-      matchB = b.match(/^((\d+)(\.(\d+))?)\s/);
-    if (matchA === null || matchB === null) return a.localeCompare(b);
-    const NumberWithoutNaN = (str: string) =>
-      isNaN(Number(str)) ? 0 : Number(str);
-    const indexA = Number(matchA[2]) * 1000 + NumberWithoutNaN(matchA[4]),
-      indexB = Number(matchB[2]) * 1000 + NumberWithoutNaN(matchB[4]);
-    return indexA - indexB;
-  }
-
   if (!fs.existsSync(dir)) {
     console.error(`Directory not found: ${dir}`);
-    return sidebar;
+    return [];
   }
   const folders = fs
     .readdirSync(dir, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory() && dirent.name !== "images")
+    .filter((dirent) => dirent.isDirectory() && !ignDirs.includes(dirent.name))
     .sort((a, b) => compareFileName(a.name, b.name));
-  const dirLastName = dir.split("/").at(-1);
+
+  const generateItems = (folderPath: string) =>
+    fs
+      .readdirSync(folderPath)
+      .filter((file) => file.endsWith(".md") && !ignFiles.includes(file))
+      .sort(compareFileName)
+      .map((file) => ({
+        text: pangu(path.basename(file, ".md")),
+        link: `/${folderPath}/${path.basename(file, ".md")}`,
+      }));
+
   if (folders.length !== 0) {
     // 文件夹模式
-    folders.forEach((folder) => {
-      const folderPath = path.join(dir, folder.name);
-      const files = fs
-        .readdirSync(folderPath)
-        .filter((file) => file.endsWith(".md"))
-        .sort(compareFileName);
-      sidebar.push({
-        text: folder.name,
-        items: files.map((file) => ({
-          text: getMarkdownTitle(path.join(folderPath, file)),
-          link: `/${dirLastName}/${folder.name}/${file}`,
-        })),
-      });
-    });
+    return folders.map((folder) => ({
+      text: pangu(folder.name),
+      items: generateItems(`${dir}/${folder.name}`),
+    }));
   } else {
     // 文件模式
-    fs.readdirSync(dir)
-      .filter((file) => file.endsWith(".md") && file !== "index.md")
-      .sort(compareFileName)
-      .forEach((file) => {
-        sidebar.push({
-          text: path.basename(file, ".md"),
-          link: dirLastName + "/" + path.basename(file, ".md"),
-        });
-      });
+    return generateItems(dir);
   }
-  return sidebar;
+}
+
+export default function sidebar() {
+  const sidebarObj = {};
+  fs.readdirSync("./", { withFileTypes: true })
+    .filter(
+      (dirent) => dirent.isDirectory() && !ignProjects.includes(dirent.name)
+    )
+    .forEach(({ name }) => (sidebarObj[name] = generateSidebar(name)));
+  return sidebarObj;
 }
