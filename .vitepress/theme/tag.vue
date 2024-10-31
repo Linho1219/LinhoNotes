d1c231410c73b1e62ba3b28f55c02a92c953bfff
Linho <lhp@linho.cc>
2024-10-31 20:24:29 +0800

    标签颜色微调

--- .vitepress/theme/tag.vue
+++ .vitepress/theme/tag.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="VPBadge" :class="BadgeClass">
+  <span class="VPBadge ln" :class="BadgeClass">
     {{ BadgeText }} <slot></slot>
   </span>
 </template>
@@ -42,90 +42,114 @@ const BadgeClass = ref(colorStr),
 </script>
 
 <style>
-.VPBadge {
+.VPBadge.ln {
   margin-left: 0px;
+  margin-right: 0px;
 }
-h1 .VPBadge,
-h2 .VPBadge,
-h3 .VPBadge,
-h4 .VPBadge,
-h5 .VPBadge,
-h6 .VPBadge {
-  margin-left: 3px;
-  margin-right: 3px;
+.VPBadge {
+  --ln-c-red: var(--vp-c-red-2);
+  --ln-c-red-soft: var(--vp-c-red-soft);
+  --ln-c-orange: #d55c37;
+  --ln-c-orange-soft: rgba(255, 146, 68, 0.16);
+  --ln-c-yellow: var(--vp-c-yellow-2);
+  --ln-c-yellow-soft: var(--vp-c-yellow-soft);
+  --ln-c-green: var(--vp-c-green-2);
+  --ln-c-green-soft: var(--vp-c-green-soft);
+  --ln-c-blue: #3573b2;
+  --ln-c-blue-soft: rgba(72, 151, 255, 0.16);
+  --ln-c-indigo: var(--vp-c-indigo-1);
+  --ln-c-indigo-soft: var(--vp-c-indigo-soft);
+  --ln-c-purple: var(--vp-c-purple-1);
+  --ln-c-purple-soft: var(--vp-c-purple-soft);
+  --ln-c-pink: var(--vp-c-red-3);
+  --ln-c-pink-soft: rgba(255, 133, 133, 0.16);
+  --ln-c-magenta: #ff53ac;
+  --ln-c-magenta-soft: rgba(255, 69, 174, 0.16);
+  --ln-c-lime: #86a822;
+  --ln-c-lime-soft: rgba(161, 197, 69, 0.16);
+  --ln-c-olive: olive;
+  --ln-c-olive-soft: rgba(100, 124, 38, 0.16);
+  --ln-c-cyan: #138aa7;
+  --ln-c-cyan-soft: rgba(44, 182, 197, 0.16);
+  --ln-c-teal: teal;
+  --ln-c-teal-soft: rgba(37, 162, 167, 0.16);
+  --ln-c-gray: var(--vp-c-text-2);
+  --ln-c-gray-soft: var(--vp-c-gray-soft);
 }
-.VPBadge,
-.VPBadge.gray {
-  color: var(--vp-c-text-2);
-  background: var(--vp-c-gray-soft);
+.dark .VPBadge {
+  --ln-c-orange: #ff7044;
+  --ln-c-yellow: var(--vp-c-yellow-1);
+  --ln-c-green:#3abf7f;
+  --ln-c-blue: #5badff;
+  --ln-c-pink: var(--vp-c-red-1);
+  --ln-c-pink-soft: rgba(255, 133, 133, 0.16);
+  --ln-c-lime: #9fc726;
+  --ln-c-olive: #baba15;
+  --ln-c-olive-soft: rgba(187, 206, 93, 0.16);
+  --ln-c-cyan: #29b8db;
+  --ln-c-cyan-soft: rgba(41, 215, 234, 0.16);
+  --ln-c-teal: #18bebe;
+  --ln-c-gray: var(--vp-c-text-2);
+  --ln-c-gray-soft: var(--vp-c-gray-soft);
 }
-.VPBadge.red {
-  color: var(--vp-c-red-2);
-  background: var(--vp-c-red-soft);
+.VPBadge.ln {
+  color: var(--ln-c-gray);
+  background: var(--ln-c-gray-soft);
 }
-.VPBadge.orange {
-  color: #ff6030;
-  background: rgba(255, 125, 33, 0.16);
+.VPBadge.ln.red {
+  color: var(--ln-c-red);
+  background: var(--ln-c-red-soft);
 }
-.VPBadge.yellow {
-  color: var(--vp-c-yellow-2);
-  background: var(--vp-c-yellow-soft);
+.VPBadge.ln.orange {
+  color: var(--ln-c-orange);
+  background: var(--ln-c-orange-soft);
 }
-.dark .VPBadge.yellow {
-  color: var(--vp-c-yellow-1);
+.VPBadge.ln.yellow {
+  color: var(--ln-c-yellow);
+  background: var(--ln-c-yellow-soft);
 }
-.VPBadge.green {
-  color: var(--vp-c-green-2);
-  background: var(--vp-c-green-soft);
+.VPBadge.ln.green {
+  color: var(--ln-c-green);
+  background: var(--ln-c-green-soft);
 }
-.VPBadge.blue {
-  color: #50a8ff;
-  background: rgba(72, 151, 255, 0.16);
+.VPBadge.ln.blue {
+  color: var(--ln-c-blue);
+  background: var(--ln-c-blue-soft);
 }
-.VPBadge.indigo {
-  color: var(--vp-c-indigo-1);
-  background: var(--vp-c-indigo-soft);
+.VPBadge.ln.indigo {
+  color: var(--ln-c-indigo);
+  background: var(--ln-c-indigo-soft);
 }
-.VPBadge.purple {
-  color: var(--vp-c-purple-1);
-  background: var(--vp-c-purple-soft);
+.VPBadge.ln.purple {
+  color: var(--ln-c-purple);
+  background: var(--ln-c-purple-soft);
 }
-.VPBadge.pink {
-  color: var(--vp-c-red-3);
-  background: rgba(255, 133, 133, 0.16);
+.VPBadge.ln.pink {
+  color: var(--ln-c-pink);
+  background: var(--ln-c-pink-soft);
 }
-.dark .VPBadge.pink {
-  color: var(--vp-c-red-1);
-  background: rgba(255, 133, 133, 0.16);
+.VPBadge.ln.magenta {
+  color: var(--ln-c-magenta);
+  background: var(--ln-c-magenta-soft);
 }
-.VPBadge.magenta {
-  color: #ff53ac;
-  background: rgba(255, 72, 148, 0.16);
+.VPBadge.ln.lime {
+  color: var(--ln-c-lime);
+  background: var(--ln-c-lime-soft);
 }
-.VPBadge.lime {
-  color: #86a822;
-  background: rgba(161, 197, 69, 0.16);
+.VPBadge.ln.olive {
+  color: var(--ln-c-olive);
+  background: var(--ln-c-olive-soft);
 }
-.dark .VPBadge.lime {
-  color: #94bb1e;
+.VPBadge.ln.cyan {
+  color: var(--ln-c-cyan);
+  background: var(--ln-c-cyan-soft);
 }
-.VPBadge.olive {
-  color: #808000;
-  background: rgba(100, 124, 38, 0.16);
+.VPBadge.ln.teal {
+  color: var(--ln-c-teal);
+  background: var(--ln-c-teal-soft);
 }
-.dark .VPBadge.olive {
-  color: #aaaa00;
-  background: rgba(187, 206, 93, 0.16);
-}
-.VPBadge.cyan {
-  color: #29b8db;
-  background: rgba(41, 215, 234, 0.16);
-}
-.VPBadge.teal {
-  color: #008080;
-  background: rgba(37, 162, 167, 0.16);
-}
-.dark .VPBadge.teal {
-  color: #00a1a1;
+.VPBadge.ln.gray {
+  color: var(--ln-c-gray);
+  background: var(--ln-c-gray-soft);
 }
 </style>
