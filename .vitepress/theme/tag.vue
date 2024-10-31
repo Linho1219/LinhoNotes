c606548329679af2ccef716c7b656290390403ad
Linho <lhp@linho.cc>
2024-10-31 20:37:54 +0800

    公共颜色定义
    把颜色定义成公共的，公式颜色也采用这套

--- .vitepress/theme/tag.vue
+++ .vitepress/theme/tag.vue
@@ -46,52 +46,6 @@ const BadgeClass = ref(colorStr),
   margin-left: 0px;
   margin-right: 0px;
 }
-.VPBadge {
-  --ln-c-red: var(--vp-c-red-2);
-  --ln-c-red-soft: var(--vp-c-red-soft);
-  --ln-c-orange: #d55c37;
-  --ln-c-orange-soft: rgba(255, 146, 68, 0.16);
-  --ln-c-yellow: var(--vp-c-yellow-2);
-  --ln-c-yellow-soft: var(--vp-c-yellow-soft);
-  --ln-c-green: var(--vp-c-green-2);
-  --ln-c-green-soft: var(--vp-c-green-soft);
-  --ln-c-blue: #3573b2;
-  --ln-c-blue-soft: rgba(72, 151, 255, 0.16);
-  --ln-c-indigo: var(--vp-c-indigo-1);
-  --ln-c-indigo-soft: var(--vp-c-indigo-soft);
-  --ln-c-purple: var(--vp-c-purple-1);
-  --ln-c-purple-soft: var(--vp-c-purple-soft);
-  --ln-c-pink: var(--vp-c-red-3);
-  --ln-c-pink-soft: rgba(255, 133, 133, 0.16);
-  --ln-c-magenta: #ff53ac;
-  --ln-c-magenta-soft: rgba(255, 69, 174, 0.16);
-  --ln-c-lime: #86a822;
-  --ln-c-lime-soft: rgba(161, 197, 69, 0.16);
-  --ln-c-olive: olive;
-  --ln-c-olive-soft: rgba(100, 124, 38, 0.16);
-  --ln-c-cyan: #138aa7;
-  --ln-c-cyan-soft: rgba(44, 182, 197, 0.16);
-  --ln-c-teal: teal;
-  --ln-c-teal-soft: rgba(37, 162, 167, 0.16);
-  --ln-c-gray: var(--vp-c-text-2);
-  --ln-c-gray-soft: var(--vp-c-gray-soft);
-}
-.dark .VPBadge {
-  --ln-c-orange: #ff7044;
-  --ln-c-yellow: var(--vp-c-yellow-1);
-  --ln-c-green:#3abf7f;
-  --ln-c-blue: #5badff;
-  --ln-c-pink: var(--vp-c-red-1);
-  --ln-c-pink-soft: rgba(255, 133, 133, 0.16);
-  --ln-c-lime: #9fc726;
-  --ln-c-olive: #baba15;
-  --ln-c-olive-soft: rgba(187, 206, 93, 0.16);
-  --ln-c-cyan: #29b8db;
-  --ln-c-cyan-soft: rgba(41, 215, 234, 0.16);
-  --ln-c-teal: #18bebe;
-  --ln-c-gray: var(--vp-c-text-2);
-  --ln-c-gray-soft: var(--vp-c-gray-soft);
-}
 .VPBadge.ln {
   color: var(--ln-c-gray);
   background: var(--ln-c-gray-soft);
