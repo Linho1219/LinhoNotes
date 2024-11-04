cdb37b84743a28a2b7db1af958f9ac8f6e16e209
Linho <lhp@linho.cc>
2024-11-04 18:45:27 +0800

    mermaid 渲染整合

--- .vitepress/codeblock/mermaid.vue
+++ .vitepress/codeblock/mermaid.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="mermaid">
-    <MermaidRender
-      :id="props.id"
-      :code="props.code"
-      :key="Number(isDark)"
-    ></MermaidRender>
+    <div v-if="!errorFlag" class="mermaid_container" v-html="svgRef"></div>
+    <div
+      v-if="errorFlag"
+      class="mermaid-error caution custom-block github-alert"
+    >
+      <p class="custom-block-title">Mermaid 渲染错误</p>
+      <pre v-html="errorDetails"></pre>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import MermaidRender from "./mermaidRender.vue";
+/// <reference path="../types.d.ts" />
 import { useData } from "vitepress";
 const { isDark } = useData();
 const props = defineProps({
   id: String,
   code: String,
 });
+
+import { ref, onMounted, watchEffect } from "vue";
+import mermaid from "mermaid";
+
+const svgRef = ref(""),
+  errorFlag = ref(false),
+  errorDetails = ref("");
+
+const code = decodeURIComponent(props.code!);
+
+onMounted(
+  () =>
+    import.meta.env.SSR ||
+    watchEffect(() => {
+      mermaid.initialize({
+        startOnLoad: false,
+        theme: isDark.value ? "dark" : "neutral",
+      });
+      mermaid.render(props.id!, code).then(
+        (result) => {
+          svgRef.value = result.svg;
+        },
+        (error) => {
+          errorFlag.value = true;
+          errorDetails.value = error.toString();
+          console.error(error);
+        }
+      );
+    })
+);
 </script>
+
+<style>
+.mermaid svg {
+  margin: 0 auto;
+}
+.mermaid-error pre {
+  white-space: pre-wrap;
+}
+</style>
