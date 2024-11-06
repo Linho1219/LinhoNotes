e30e93badc95f2fcb4fd03c4797991171473b313
Linho <linho1219@outlook.com>
2024-11-06 16:53:43 +0800

    改动 Mermaid 图暗色下渲染
    暗色模式改为使用滤镜实现；切换颜色模式不再重新渲染

--- .vitepress/codeblock/mermaid.vue
+++ .vitepress/codeblock/mermaid.vue
@@ -10,10 +10,11 @@
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 /// <reference path="../types.d.ts" />
 import { useData } from "vitepress";
-const { isDark } = useData();
+
 const props = defineProps({
   id: String,
   code: String,
@@ -28,33 +29,42 @@ const svgRef = ref(""),
 
 const code = decodeURIComponent(props.code!);
 
-onMounted(
-  () =>
-    import.meta.env.SSR ||
-    watchEffect(() => {
-      mermaid.initialize({
-        startOnLoad: false,
-        theme: isDark.value ? "dark" : "neutral",
-      });
-      mermaid.render(props.id!, code).then(
-        (result) => {
-          svgRef.value = result.svg;
-        },
-        (error) => {
-          errorFlag.value = true;
-          errorDetails.value = error.toString();
-          console.error(error);
-        }
-      );
-    })
-);
+if (!import.meta.env.SSR) {
+  mermaid.initialize({
+    startOnLoad: false,
+    theme: "neutral",
+    fontFamily: "var(--vp-font-family-base)",
+  });
+  mermaid.render(props.id!, code).then(
+    (result) => {
+      svgRef.value = result.svg;
+    },
+    (error) => {
+      errorFlag.value = true;
+      errorDetails.value = error.toString();
+      console.error(error);
+    }
+  );
+}
 </script>
 
 <style>
 .mermaid svg {
   margin: 0 auto;
 }
+.mermaid_container,
+.mermaid_container p {
+  line-height: 1.5em !important;
+  user-select: none;
+}
 .mermaid-error pre {
   white-space: pre-wrap;
+}
+.dark .mermaid_container {
+  filter: invert() contrast(80%);
+  text-shadow: #fff 0 0 3px, #fff 0 0 8px;
+}
+.dark .mermaid_container text {
+  fill: #000 !important;
 }
 </style>
