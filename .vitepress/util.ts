69c11c0231bbb76bd27f7916d3a7f89a297a4543
Linho <lhp@linho.cc>
2024-11-15 01:53:21 +0800

    贡献者列表
    后面准备做文章底下的贡献者列表

--- .vitepress/util.ts
+++ .vitepress/util.ts
@@ -4,3 +4,31 @@ export const pangu = (str: string) =>
     .replace(/([\u4e00-\u9fff])([0-9A-Za-z&+\-])/g, "$1 $2");
 
 export const baseUrl = "https://notes.linho.cc";
+
+type Contributor = {
+  username: string;
+  emailHash: string[];
+};
+
+export const contributors: Contributor[] = [
+  {
+    username: "Linho",
+    emailHash: ["81c7445a52", "0c5b28c9ad"],
+  },
+  {
+    username: "Horrible120",
+    emailHash: ["fcf0370694"],
+  },
+  {
+    username: "SkinCrab",
+    emailHash: ["7368906deb"],
+  },
+  {
+    username: "acha666",
+    emailHash: ["d772a76fa1"],
+  },
+  {
+    username: "CindyFang0716",
+    emailHash: ["2e08b8e801"],
+  },
+];
