f990bb2266f48ef5de5c4e47e01082889924e539
Linho <lhp@linho.cc>
2024-11-15 20:28:24 +0800

    调整头像下载逻辑

--- .vitepress/mirrorAvartar.ts
+++ .vitepress/mirrorAvartar.ts
@@ -8,6 +8,10 @@ async function downloadImage(url: string, savePath: string) {
     url,
     method: "GET",
     responseType: "stream",
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+    },
   });
   response.data.pipe(writer);
   return new Promise((resolve, reject) => {
@@ -16,16 +20,30 @@ async function downloadImage(url: string, savePath: string) {
   });
 }
 
-export default function mirrorAvartar() {
-  for (const { username } of fullContributorList) {
-    console.log(username);
-    fs.mkdirSync("./public/avartars", { recursive: true });
-    downloadImage(
-      `https://github.com/${username}.png`,
-      `./public/avartars/${username}.png`
-    ).then(
-      () => console.log(`${username}.png downloaded successfully`),
-      (err) => console.error(`Error downloading ${username}.png: ${err}`)
-    );
-  }
+export default async function mirrorAvartar() {
+  let cnt = 0;
+  return new Promise((resolve) => {
+    for (const { username } of fullContributorList) {
+      console.log(username);
+      fs.mkdirSync("./public/avartars", { recursive: true });
+      cnt++;
+      downloadImage(
+        `https://github.com/${username}.png`,
+        `./public/avartars/${username}.png`
+      )
+        .then(
+          () => {
+            console.log(`${username}.png downloaded successfully`);
+            cnt--;
+          },
+          (err) => {
+            console.error(`Error downloading ${username}.png: ${err}`);
+            cnt--;
+          }
+        )
+        .finally(() => {
+          if (cnt === 0) resolve(null);
+        });
+    }
+  });
 }
