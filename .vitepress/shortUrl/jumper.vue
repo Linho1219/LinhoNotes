116f1c6993440f24e8d2e546598a068622e27eea
Linho <linho1219@outlook.com>
2024-11-20 17:03:20 +0800

    逻辑优化

--- .vitepress/shortUrl/jumper.vue
+++ .vitepress/shortUrl/jumper.vue
@@ -1,22 +1,17 @@
-<template></template>
-
 <script setup lang="ts">
 import axios from "axios";
 import { onMounted } from "vue";
 import { useRouter } from "vitepress";
-
 const router = useRouter();
 onMounted(() => {
   const id = window.location.search.match(/\?q=(.{10})$/)?.[1];
-  if (id)
-    axios.get("/shortmap.json").then(
-      (res) => {
-        if (typeof res.data[id] !== "undefined")
-          router.go(`./${encodeURI(res.data[id])}`);
-        else router.go(`./404`);
-      },
-      () => router.go(`./404`)
-    );
-  else router.go(`./404`);
+  if (!id) return router.go(`./404`);
+  axios.get("/shortmap.json").then(
+    (res) =>
+      res.data[id] !== undefined
+        ? router.go(`./${encodeURI(res.data[id])}`)
+        : router.go(`./404`),
+    () => router.go(`./404`)
+  );
 });
 </script>
