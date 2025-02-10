d079cddb3c69eeeb17e41faac1d15acb0d0cd3f6
Linho <linho1219@outlook.com>
2025-02-10 23:06:37 +0800

    Revert "CSS 数据取整"
    This reverts commit f0d5d1823c6b0f4d62f2c1313d7d535966acd4fb.

--- .vitepress/imageViewer/imageViewer.vue
+++ .vitepress/imageViewer/imageViewer.vue
@@ -89,9 +89,7 @@ const style = computed(
   (): StyleValue => ({
     width: props.initWidth! + "px",
     height: props.initHeight! + "px",
-    transform: `translate(${Math.round(position.x - props.initWidth! / 2)}px,
-      ${Math.round(position.y - props.initHeight! / 2)}px)
-      scale(${Math.round(scale.value * 100)}%)`,
+    transform: `translate(${position.x - props.initWidth! / 2}px,${position.y - props.initHeight! / 2}px) scale(${scale.value})`,
   })
 );
 const transitionEnabled = ref(false);
