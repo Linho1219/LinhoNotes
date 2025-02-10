d334d860b2db5bf727c8df2f0665049bddcb2558
Linho <linho1219@outlook.com>
2025-02-10 16:24:26 +0800

    Merge branch: 格式化 URL (#7)
    * 格式化 URL
    
    * 网页部件空格处理
    
    * 404 页面 fallback 处理
    
    * 修复死链

---
+++ .vitepress/shortUrl/notFound.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useRouter } from "vitepress";
+const router = useRouter();
+onMounted(() => {
+  const url = window.location.href;
+  if (url.includes("%20")) router.go(url.replaceAll("%20", "-"));
+});
+</script>
