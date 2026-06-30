4b0e745950df0d339dfa9bc668846b47012fe905
Linho <lhp@linho.cc>
2026-06-30 20:49:18 +0800

    黑幕允许键盘聚焦

--- .vitepress/theme/components/cloze.vue
+++ .vitepress/theme/components/cloze.vue
@@ -1,20 +1,21 @@
 <template>
-  <span class="cloze"><slot></slot></span>
+  <span class="cloze" tabindex="0"><slot></slot></span>
 </template>
 <style>
 .cloze {
-  --cloze-transition: 0.15s;
+  --cloze-transition-duration: 0.15s;
   background: #888;
   border-radius: 2px;
   transition:
-    filter var(--cloze-transition),
-    background-color var(--cloze-transition),
-    opacity var(--cloze-transition);
+    filter var(--cloze-transition-duration),
+    background-color var(--cloze-transition-duration),
+    opacity var(--cloze-transition-duration);
   filter: contrast(0);
   opacity: 0.4;
   padding: 2px 0;
 }
-.cloze:hover {
+.cloze:hover,
+.cloze:focus-visible {
   filter: contrast(1);
   opacity: 1;
   background: #8882;
