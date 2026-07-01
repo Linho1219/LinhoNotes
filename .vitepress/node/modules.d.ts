56bbeb9114ef93a1977ba8cf779b90f6b1d3bfb3
Linho <lhp@linho.cc>
2026-07-01 16:27:23 +0800

    基础类型结构

---
+++ .vitepress/node/modules.d.ts
@@ -0,0 +1,14 @@
+declare module "markdown-it-task-lists" {
+  import type MarkdownIt from "markdown-it";
+  export default function (md: MarkdownIt): void;
+}
+
+declare module "markdown-it-sup" {
+  import type MarkdownIt from "markdown-it";
+  export default function (md: MarkdownIt): void;
+}
+
+declare module "markdown-it-sub" {
+  import type MarkdownIt from "markdown-it";
+  export default function (md: MarkdownIt): void;
+}
--- .vitepress/types.d.ts
+++ .vitepress/types.d.ts
@@ -1,20 +1,3 @@
-/// <reference types="vite/client" />
-
-declare module "markdown-it-task-lists" {
-  import type MarkdownIt from "markdown-it";
-  export default function (md: MarkdownIt): void;
-}
-
-declare module "markdown-it-sup" {
-  import type MarkdownIt from "markdown-it";
-  export default function (md: MarkdownIt): void;
-}
-
-declare module "markdown-it-sub" {
-  import type MarkdownIt from "markdown-it";
-  export default function (md: MarkdownIt): void;
-}
-
 interface ImportMetaEnv {
   /** 应用运行的模式 */
   MODE: string;
