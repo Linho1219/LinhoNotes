60986377783274519d58fbf1c62bb4571fc9c17f
Linho <linho1219@outlook.com>
2026-09-24 15:19:14 +0800

    调色板调整增补

--- .vitepress/theme/components/tag.vue
+++ .vitepress/theme/components/tag.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script setup lang="ts">
+import { PaletteJSON } from '@theme/features/utils/palette'
 import { ref, useAttrs } from 'vue'
 
 const props = defineProps<{
@@ -11,22 +12,7 @@ const props = defineProps<{
   text?: string
   t?: string
 }>()
-const colorNames = [
-  'red',
-  'orange',
-  'yellow',
-  'green',
-  'blue',
-  'indigo',
-  'purple',
-  'pink',
-  'magenta',
-  'lime',
-  'olive',
-  'cyan',
-  'teal',
-  'gray',
-]
+const colorNames = Object.keys(PaletteJSON.light.text)
 
 let colorStr = ''
 const attrs = useAttrs()
@@ -39,69 +25,22 @@ const BadgeClass = ref(colorStr),
   BadgeText = ref(props.text ?? props.t)
 </script>
 
-<style>
+<style scoped lang="scss">
 .VPBadge.ln {
   margin-left: 0px;
   margin-right: 0px;
-}
-.VPBadge.ln {
   color: var(--ln-c-gray);
   background: var(--ln-c-gray-soft);
 }
-.VPBadge.ln.red {
-  color: var(--ln-c-red);
-  background: var(--ln-c-red-soft);
-}
-.VPBadge.ln.orange {
-  color: var(--ln-c-orange);
-  background: var(--ln-c-orange-soft);
-}
-.VPBadge.ln.yellow {
-  color: var(--ln-c-yellow);
-  background: var(--ln-c-yellow-soft);
-}
-.VPBadge.ln.green {
-  color: var(--ln-c-green);
-  background: var(--ln-c-green-soft);
-}
-.VPBadge.ln.blue {
-  color: var(--ln-c-blue);
-  background: var(--ln-c-blue-soft);
-}
-.VPBadge.ln.indigo {
-  color: var(--ln-c-indigo);
-  background: var(--ln-c-indigo-soft);
-}
-.VPBadge.ln.purple {
-  color: var(--ln-c-purple);
-  background: var(--ln-c-purple-soft);
-}
-.VPBadge.ln.pink {
-  color: var(--ln-c-pink);
-  background: var(--ln-c-pink-soft);
-}
-.VPBadge.ln.magenta {
-  color: var(--ln-c-magenta);
-  background: var(--ln-c-magenta-soft);
-}
-.VPBadge.ln.lime {
-  color: var(--ln-c-lime);
-  background: var(--ln-c-lime-soft);
-}
-.VPBadge.ln.olive {
-  color: var(--ln-c-olive);
-  background: var(--ln-c-olive-soft);
-}
-.VPBadge.ln.cyan {
-  color: var(--ln-c-cyan);
-  background: var(--ln-c-cyan-soft);
-}
-.VPBadge.ln.teal {
-  color: var(--ln-c-teal);
-  background: var(--ln-c-teal-soft);
-}
-.VPBadge.ln.gray {
-  color: var(--ln-c-gray);
-  background: var(--ln-c-gray-soft);
+
+$colors:
+  red, orange, yellow, green, blue, indigo, purple, pink, magenta, lime, olive, cyan, teal, claret,
+  brown, gray;
+
+@each $color in $colors {
+  .VPBadge.ln.#{$color} {
+    color: var(--ln-c-#{$color});
+    background: var(--ln-c-#{$color}-soft);
+  }
 }
 </style>
